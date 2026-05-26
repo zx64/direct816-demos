867f11b04393bdf28ef31373b3cab6ed073c7063
zx64 <zx64@users.noreply.github.com>
2026-05-26 17:19:16 +0100

    Adapt tufty2350 CI script to building native modules

--- ci/native_modules.sh
+++ ci/native_modules.sh
@@ -1,17 +1,7 @@
 export TERM=${TERM:="xterm-256color"}
 
-# cache buster: 2025-10-08
-
 MICROPYTHON_FLAVOUR="zx64"
 MICROPYTHON_VERSION="bw-1.28.0"
-
-PIMORONI_PICO_FLAVOUR="pimoroni"
-PIMORONI_PICO_VERSION="37a1b6500f77924b2a3287009734bb24d4809bf1"
-
-PY_DECL_VERSION="v0.0.5"
-DIR2UF2_VERSION="v0.1.0"
-FFSMAKE_VERSION="v0.0.3"
-
 
 function log_success {
 	echo -e "$(tput setaf 2)$1$(tput sgr0)"
@@ -25,39 +15,10 @@ function log_warning {
 	echo -e "$(tput setaf 1)$1$(tput sgr0)"
 }
 
-function ci_pimoroni_pico_clone {
-    log_inform "Using Pimoroni Pico $PIMORONI_PICO_FLAVOUR/$PIMORONI_PICO_VERSION"
-    git clone https://github.com/$PIMORONI_PICO_FLAVOUR/pimoroni-pico "$CI_BUILD_ROOT/pimoroni-pico"
-    git -C "$CI_BUILD_ROOT/pimoroni-pico" checkout $PIMORONI_PICO_VERSION
-    git -C "$CI_BUILD_ROOT/pimoroni-pico" submodule update --init
-}
-
 function ci_micropython_clone {
     log_inform "Using MicroPython $MICROPYTHON_FLAVOUR/$MICROPYTHON_VERSION"
     git clone https://github.com/$MICROPYTHON_FLAVOUR/micropython "$CI_BUILD_ROOT/micropython"
     git -C "$CI_BUILD_ROOT/micropython" checkout $MICROPYTHON_VERSION
-    git -C "$CI_BUILD_ROOT/micropython" submodule update --init lib/pico-sdk
-    git -C "$CI_BUILD_ROOT/micropython" submodule update --init lib/cyw43-driver
-    git -C "$CI_BUILD_ROOT/micropython" submodule update --init lib/lwip
-    git -C "$CI_BUILD_ROOT/micropython" submodule update --init lib/mbedtls
-    git -C "$CI_BUILD_ROOT/micropython" submodule update --init lib/micropython-lib
-    git -C "$CI_BUILD_ROOT/micropython" submodule update --init lib/tinyusb
-    git -C "$CI_BUILD_ROOT/micropython" submodule update --init lib/btstack
-    git -C "$CI_BUILD_ROOT/micropython/lib/pico-sdk" apply "$CI_PROJECT_ROOT/ci/pico-sdk-crt0-startup-rosc.patch"
-}
-
-function ci_tools_clone {
-    mkdir -p "$CI_BUILD_ROOT/tools"
-    git clone https://github.com/gadgetoid/py_decl -b "$PY_DECL_VERSION" "$CI_BUILD_ROOT/tools/py_decl"
-    git clone https://github.com/gadgetoid/dir2uf2 -b "$DIR2UF2_VERSION" "$CI_BUILD_ROOT/tools/dir2uf2"
-    git clone https://github.com/gadgetoid/ffsmake -b "$FFSMAKE_VERSION" "$CI_BUILD_ROOT/tools/ffsmake" --recursive
-
-    # Build FFSMake utility
-    FFSMAKE_DIR="$CI_BUILD_ROOT/tools/ffsmake"
-    # git apply --directory="$FFSMAKE_DIR/oofatfs" "$FFSMAKE_DIR/n_fats.patch"
-    mkdir -p "$FFSMAKE_DIR/build"
-    cmake -S "$FFSMAKE_DIR" -B "$FFSMAKE_DIR/build"
-    cmake --build "$FFSMAKE_DIR/build"
 }
 
 function ci_micropython_build_mpy_cross {
@@ -72,13 +33,11 @@ function ci_apt_install_build_deps {
 
 function ci_install_build_deps {
     ci_apt_install_build_deps
-    python3 -m pip install littlefs-python==0.12.0
 }
 
 function ci_prepare_all {
     ci_tools_clone
     ci_micropython_clone
-    ci_pimoroni_pico_clone
     ci_micropython_build_mpy_cross
 }
 
@@ -87,80 +46,10 @@ function ci_debug {
     log_inform "Build root: $CI_BUILD_ROOT"
 }
 
-function micropython_version {
-    BOARD=$1
-    echo "MICROPY_GIT_TAG=$MICROPYTHON_VERSION, $BOARD $TAG_OR_SHA" >> $GITHUB_ENV
-    echo "MICROPY_GIT_HASH=$MICROPYTHON_VERSION-$TAG_OR_SHA" >> $GITHUB_ENV
-}
-
-function ci_genversion {
-    BOARD="tufty"
-    MICROPY_BOARD_DIR=$CI_PROJECT_ROOT/board
-    if [ -z ${CI_RELEASE_FILENAME+x} ]; then
-        CI_RELEASE_FILENAME=$BOARD
-    fi
-
-    MICROPYTHON_SHA=`git -C "$CI_BUILD_ROOT/micropython" describe --always --long --abbrev=40 HEAD`
-    PIMORONI_PICO_SHA=`git -C "$CI_BUILD_ROOT/pimoroni-pico" describe --always --long --abbrev=40 HEAD`
-    RELEASE_FILE="$CI_RELEASE_FILENAME"
-
-    cat << EOF > "$MICROPY_BOARD_DIR/version.py"
-DATE="`date`"
-BUILD="$RELEASE_FILE"
-MICROPYTHON_SHA="$MICROPYTHON_SHA"
-PIMORONI_PICO_SHA="$PIMORONI_PICO_SHA"
-EOF
-}
-
-function ci_cmake_configure {
-    BOARD="tufty"
-    TOOLS_DIR="$CI_BUILD_ROOT/tools"
-    MICROPY_BOARD_DIR="$CI_PROJECT_ROOT/board"
-    if [ ! -f "$MICROPY_BOARD_DIR/mpconfigboard.h" ]; then
-        log_warning "Invalid board: \"$BOARD\". Run with ci_cmake_configure <board_name>."
-        return 1
-    fi
-    BUILD_DIR="$CI_BUILD_ROOT/build-$BOARD"
-    cmake -S $CI_BUILD_ROOT/micropython/ports/rp2 -B "$BUILD_DIR" \
-    -DPICOTOOL_FORCE_FETCH_FROM_GIT=1 \
-    -DPICO_BUILD_DOCS=0 \
-    -DPICO_NO_COPRO_DIS=1 \
-    -DPICOTOOL_FETCH_FROM_GIT_PATH="$TOOLS_DIR/picotool" \
-    -DPIMORONI_PICO_PATH="$CI_BUILD_ROOT/pimoroni-pico" \
-    -DPIMORONI_TOOLS_DIR="$TOOLS_DIR" \
-    -DUSER_C_MODULES="$MICROPY_BOARD_DIR/usermodules.cmake" \
-    -DMICROPY_BOARD_DIR="$MICROPY_BOARD_DIR" \
-    -DMICROPY_BOARD="$BOARD" \
-    -DCMAKE_C_COMPILER_LAUNCHER=ccache \
-    -DCMAKE_CXX_COMPILER_LAUNCHER=ccache
-}
-
-function ci_cmake_build {
-    BOARD="tufty"
-    MICROPY_BOARD_DIR="$CI_PROJECT_ROOT/board"
-    if [ ! -f "$MICROPY_BOARD_DIR/mpconfigboard.h" ]; then
-        log_warning "Invalid board: \"$BOARD\". Run with ci_cmake_build <board_name>."
-        return 1
-    fi
-
-    ci_genversion $BOARD
-
-    BUILD_DIR="$CI_BUILD_ROOT/build-$BOARD"
+function ci_build {
     ccache --zero-stats || true
-    cmake --build $BUILD_DIR -j ${CMAKE_BUILD_PARALLEL_LEVEL:-2} || return 1
+    CROSS_COMPILE="ccache" MPY_DIR="$CI_BUILD_ROOT/micropython" make -C "$CI_PROJECT_ROOT/natmod" || return 1
     ccache --show-stats || true
-
-    if [ -z ${CI_RELEASE_FILENAME+x} ]; then
-        CI_RELEASE_FILENAME=$BOARD
-    fi
-
-    log_inform "Copying -romfs.uf2 to $(pwd)/$CI_RELEASE_FILENAME.uf2"
-    cp "$BUILD_DIR/firmware-romfs.uf2" $CI_RELEASE_FILENAME.uf2
-
-    if [ -f "$BUILD_DIR/firmware-romfs-with-filesystem.uf2" ]; then
-        log_inform "Copying romfs-with-filesystem .uf2 to $(pwd)/$CI_RELEASE_FILENAME-with-filesystem.uf2"
-        cp "$BUILD_DIR/firmware-romfs-with-filesystem.uf2" $CI_RELEASE_FILENAME-with-filesystem.uf2
-    fi
 }
 
 if [ -z ${CI_USE_ENV+x} ] || [ -z ${CI_PROJECT_ROOT+x} ] || [ -z ${CI_BUILD_ROOT+x} ]; then
