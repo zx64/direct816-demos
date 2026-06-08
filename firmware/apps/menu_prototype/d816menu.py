c9698979f5a93321aeb223d6ac5f6c8922462c3a
zx64 <zx64@users.noreply.github.com>
2026-06-08 22:11:10 +0100

    Change menu layout into three rows

--- firmware/apps/menu_prototype/d816menu.py
+++ firmware/apps/menu_prototype/d816menu.py
@@ -13,7 +13,7 @@ def init():
     screen.font = rom_font.nope
     font_width, font_height = screen.measure_text("W")
 
-    global background_effect_names, foreground_effect_names, palette_names
+    global background_effect_names, foreground_effect_names, palette_names, num_cols
 
     background_effect_names = [
         "palette cycling",
@@ -30,11 +30,18 @@ def init():
     except OSError:
         palette_names = ["default"]
 
-    global effect_sprites, preview_palette_strips, full_palette_strips
+    num_cols = [
+        len(background_effect_names),
+        len(foreground_effect_names),
+        len(palette_names),
+    ]
+
+    global effect_sprites, preview_palette_strips, full_palette_strips, ui_labels
     effect_sprites = {
         name: make_text_sprite(name)
         for name in set(background_effect_names + foreground_effect_names)
     }
+    ui_labels = [make_text_sprite(text) for text in ["bg: ", "fg: ", "pal: "]]
 
     preview_palette_strips = [
         make_palette_strip(name, strip_width, strip_height) for name in palette_names
@@ -80,35 +87,36 @@ def make_palette_strip(name, w, h):
     return img
 
 
+num_ui_rows = const(3)
+
+
 class D816Menu:
     def __init__(self):
-        self.effect_idx = 0
-        self.palette_idx = 0
+        self.selections = [0, 0, 0]
+        self.ui_row = 0
         self.visible = True
 
     def up(self):
         if not self.visible:
             self.visible = True
-        self.palette_idx = (self.palette_idx - 1) % len(palette_names)
-        print(palette_names[self.palette_idx])
+        self.ui_row = (self.ui_row - 1) % num_ui_rows
 
     def down(self):
         if not self.visible:
             self.visible = True
-        self.palette_idx = (self.palette_idx + 1) % len(palette_names)
-        print(palette_names[self.palette_idx])
+        self.ui_row = (self.ui_row + 1) % num_ui_rows
 
     def left(self):
         if not self.visible:
             self.visible = True
-        self.effect_idx = (self.effect_idx - 1) % len(background_effect_names)
-        print(background_effect_names[self.effect_idx])
+        row = self.ui_row
+        self.selections[row] = (self.selections[row] - 1) % num_cols[row]
 
     def right(self):
         if not self.visible:
             self.visible = True
-        self.effect_idx = (self.effect_idx + 1) % len(background_effect_names)
-        print(background_effect_names[self.effect_idx])
+        row = self.ui_row
+        self.selections[row] = (self.selections[row] + 1) % num_cols[row]
 
     def ok(self):
         self.visible = not self.visible
@@ -117,24 +125,42 @@ class D816Menu:
         if not self.visible:
             return
 
-        # Effects selector
-        screen.pen = color.grey
         x, y = 0, 3 * screen.height // 4 - font_height // 2
+        selected_row_y = y + self.ui_row * font_height
+        screen.pen = color.green
+        screen.rectangle(0, selected_row_y, screen.width, font_height)
+        screen.pen = color.orange
+
+        # Effects selectors
+        screen.blit(ui_labels[0], vec2(x, y))
+        x += ui_labels[0].width
         for idx, name in enumerate(background_effect_names):
             spr = effect_sprites[name]
-            if idx == self.effect_idx:
+            if idx == self.selections[0]:
+                screen.rectangle(x, y, spr.width, spr.height)
+            screen.blit(effect_sprites[name], vec2(x, y))
+            x += spr.width + font_width
+
+        x = 0
+        y += font_height
+        screen.blit(ui_labels[1], vec2(x, y))
+        x += ui_labels[1].width
+        for idx, name in enumerate(foreground_effect_names):
+            spr = effect_sprites[name]
+            if idx == self.selections[1]:
                 screen.rectangle(x, y, spr.width, spr.height)
             screen.blit(effect_sprites[name], vec2(x, y))
             x += spr.width + font_width
 
         # Palette selector
-        r = rect(screen.width - strip_width - 2, 0, strip_width, strip_height)
+        x = 0
+        y += font_height
+        screen.blit(ui_labels[2], vec2(x, y))
+        x += ui_labels[2].width
         for idx, preview in enumerate(preview_palette_strips):
-            if idx == self.palette_idx:
-                screen.blit(
-                    full_palette_strips[idx],
-                    rect(screen.width - 256, r.y, 256, strip_height),
-                )
+            if idx == self.selections[2]:
+                spr = full_palette_strips[idx]
             else:
-                screen.blit(preview, r)
-            r.y += strip_height
+                spr = preview
+            screen.blit(spr, vec2(x, y))
+            x += spr.width + font_width
