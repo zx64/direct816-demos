07ee86ca679ca34de53864b58414542c0a112358
zx64 <zx64@users.noreply.github.com>
2026-06-09 20:50:31 +0100

    More ideas

--- tools/palette_conv.py
+++ tools/palette_conv.py
@@ -5,7 +5,8 @@ from array import array  # noqa: F401
 # TODO:
 # Extract my palette conversion code from other projects
 #
-# Generate Doom-era palette-to-palette lookups for things like brightness adjusting or
-# finding the best match for 50% transluscency between two colours
+# Generate Doom-era palette-to-palette lookups for things like brightness adjusting,
+# applying tints, full screen effects or finding the best match for 50% transluscency
+# between two colours
 #
 # Palette expansion for smaller palettes (smoother blends etc.)
