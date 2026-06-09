6b243c964d12fcfa45c238146ae65d59cff81f0e
zx64 <zx64@users.noreply.github.com>
2026-06-09 20:48:51 +0100

    Stub files for two important host side tools

---
+++ tools/image_conv.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python
+
+from PIL import Image  # noqa: F401
+
+# TODO:
+# Everything!
+# Load images, rotate and convert to Direct8 or Direct16 format with header data
+# Generate atlases
+# Generate palette from source images if possible
---
+++ tools/palette_conv.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python
+
+from array import array  # noqa: F401
+
+# TODO:
+# Extract my palette conversion code from other projects
+#
+# Generate Doom-era palette-to-palette lookups for things like brightness adjusting or
+# finding the best match for 50% transluscency between two colours
+#
+# Palette expansion for smaller palettes (smoother blends etc.)
