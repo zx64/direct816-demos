3612cef5012b049345d4e5094e098cb32b4ea4a3
zx64 <zx64@users.noreply.github.com>
2026-06-21 21:27:36 +0100

    Small collection of ways to produce pre-sized arrays. bytearray is fastest but custom_iterator has a good tradeoff of speed against temporary allocations.

--- wip/zeroes.py
+++ wip/zeroes.py
@@ -1,6 +1,5 @@
 import array
 import gc
-import micropython
 import time
 
 
@@ -8,29 +7,34 @@ def benchmark(f):
     name = f.__name__[5:]
 
     def mf(n):
-        print(f"              {name} start")
+        memsnapshot = [0, 0, 0]
+        expected_use = n * 2
+        print(f"-----------------\n            {name}")
         gc.collect()
-        micropython.mem_info()
+        memsnapshot[0] = gc.mem_alloc()
+        print("running...", end="\r")
         start = time.ticks_us()
         r = f(n)
         duration = time.ticks_diff(time.ticks_us(), start) / 1000
-        print(f"              {name} {duration=:.2f}ms")
+        print(f"duration: {duration:.2f}ms ({1000 * duration / n:.2f}us per n)")
+        memsnapshot[1] = gc.mem_alloc()
         gc.collect()
-        micropython.mem_info()
-        print("-----------------")
+        memsnapshot[2] = gc.mem_alloc()
+        print(f"temp allocations: {memsnapshot[1] - memsnapshot[2]} bytes")
+        print(f"final overhead: {memsnapshot[2] - memsnapshot[0] - expected_use} bytes")
         return r
 
     return mf
 
 
 @benchmark
-def test_baseline(n):
-    """Baseline using a native range (doesn't zero init)"""
+def test_range_nonzero(n):
+    """Use a native range (doesn't zero init)"""
     return array.array("H", range(n))
 
 
 @benchmark
-def test_existing(n):
+def test_grown_list(n):
     """Existing implementation where a list is grown to size before handing off to array"""
 
     def z(n):
@@ -43,6 +47,12 @@ def test_existing(n):
 def test_bytearray(n):
     """Test allocating with bytearray"""
     return array.array("H", bytearray(n * 2))
+
+
+@benchmark
+def test_bytes(n):
+    """Test allocating with bytes"""
+    return array.array("H", bytes(n * 2))
 
 
 @benchmark
@@ -75,6 +85,8 @@ def test_generator_function(n):
 
 
 fbsize = const(320 * 240 * 2)
-for f in (v for k, v in globals().items() if k.startswith("test_")):
-    arr = f(fbsize)
+for _, func in sorted(
+    (name, func) for (name, func) in globals().items() if name.startswith("test_")
+):
+    arr = func(fbsize)
     assert len(arr) == fbsize
